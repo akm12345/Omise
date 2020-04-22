68c9cfa6d5abbed2d047ffe9ceb0ea64407c6e52
Amal Mishra <amalm@mindfiresolutions.com>
2020-04-22 19:23:00 +0530

    * Folder Restructure * Remove the default comments from AppDelegate and SceneDelegate * Move AppDelegate and SceneDelegate to App folder

---
+++ Omise/App/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  Omise
+//
+//  Created by Amal Mishra on 22/04/20.
+//  Copyright © 2020 Amal Mishra. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
