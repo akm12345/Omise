85b99c744bea40a72eafce9f8b8603424fc03dd1
Amal Mishra <amalm@mindfiresolutions.com>
2020-04-22 18:29:42 +0530

    Initial project setup

---
+++ Omise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Omise
+//
+//  Created by Amal Mishra on 22/04/20.
+//  Copyright © 2020 Amal Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
